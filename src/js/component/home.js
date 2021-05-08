cd446fe5c9795610135c7d94a2cc4025038734f6
sabjavi <jasabal@hotmail.com>
2021-05-08 04:12:11 +0000

    todo list con fetch actualizado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,7 +6,7 @@ import { Todo } from "./todoList.js";
 
 //create your first component
 export const Home = () => {
-	let myApi = "https://assets.breatheco.de/apis/fake/todos/user/jgarciaf106";
+	let myApi = "https://assets.breatheco.de/apis/fake/todos/user/jsaborio";
 	return (
 		<div className="text-center mx-auto mt-5 w-25 rounded border border-info">
 			<Header />
